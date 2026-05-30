26754a9d2316b6748b33bd3aa0bbc3c1b295c16b
Ivan <vanas0576@gmail.com>
2026-05-30 14:56:23 +0500

    add login

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView 
+from django.views.generic import TemplateView
+from users.views import UserLoginView, UserLogoutView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('users/', include('users.urls')), 
-
-    path('', TemplateView.as_view(template_name='index.html'), name='home'), 
+    path('users/', include('users.urls')),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -7,6 +7,8 @@ from django.urls import reverse_lazy
 from django.contrib.messages.views import SuccessMessageMixin
 from django.utils.translation import gettext_lazy as _
 from .forms import RegisterUserForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib import messages
 
 class UserListView(ListView):
     model = User
@@ -18,3 +20,15 @@ class UserCreateView(SuccessMessageMixin, CreateView):
     form_class = RegisterUserForm
     success_url = reverse_lazy('users:index')
     success_message = _('Пользователь успешно зарегистрирован')
+
+class UserLoginView(SuccessMessageMixin, LoginView):
+    template_name = 'users/login.html'
+    next_page = reverse_lazy('home')
+    success_message = _('Вы залогинены')
+
+class UserLogoutView(LogoutView):
+    next_page = reverse_lazy('home')
+
+    def dispatch(self, request, *args, **kwargs):
+        messages.info(request, _('Вы разлогинены'))
+        return super().dispatch(request, *args, **kwargs)
