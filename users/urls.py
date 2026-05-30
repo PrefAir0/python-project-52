0b3a8e1ce1204831e697f32cf8d731a9b7f9fa00
Ivan <vanas0576@gmail.com>
2026-05-30 11:37:17 +0500

    add users table

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ app_name = 'users'
 
 urlpatterns = [
     path('', views.UserListView.as_view(), name='index'),
+    path('create/', views.UserCreateView.as_view(), name='create'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.contrib.auth.models import User
+from django.views.generic import ListView, CreateView
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
+from django.utils.translation import gettext_lazy as _
+from .forms import RegisterUserForm
 
 class UserListView(ListView):
     model = User
     template_name = 'users/index.html'
     context_object_name = 'users'
+
+class UserCreateView(SuccessMessageMixin, CreateView):
+    template_name = 'users/create.html'
+    form_class = RegisterUserForm
+    success_url = reverse_lazy('users:index')
+    success_message = _('Пользователь успешно зарегистрирован')
