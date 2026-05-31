edcc0da20660184bd51b84f008d2f1ffc331b4ad
Ivan <vanas0576@gmail.com>
2026-05-31 21:08:04 +0500

    add triger error

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -2,14 +2,22 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from users.views import UserLoginView, UserLogoutView
+from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+
+
+def test_error(request):
+    division_by_zero = 1 / 0
+    return division_by_zero
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
+    path('test-error/', test_error),
     path('users/', include('users.urls')),
     path('statuses/', include('statuses.urls')),
     path('tasks/', include('tasks.urls')),
     path('labels/', include('labels.urls')),
-    path('', TemplateView.as_view(template_name='index.html'), name='home'),
-    path('login/', UserLoginView.as_view(), name='login'),
-    path('logout/', UserLogoutView.as_view(), name='logout'),
+    
 ]
