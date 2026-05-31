b8e2c56714e1347da015a97b73557dfcc238991f
Ivan <vanas0576@gmail.com>
2026-05-31 19:47:24 +0500

    fix links

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -2,16 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from users.views import UserLoginView, UserLogoutView
-from task_manager import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('statuses/', include('statuses.urls')),
+    path('tasks/', include('tasks.urls')),
+    path('labels/', include('labels.urls')),
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
-    path('tasks/', include('tasks.urls')),
-    path('labels/', include('labels.urls')),
-    path('admin/', admin.site.urls),
 ]
